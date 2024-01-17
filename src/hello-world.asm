faa013117f20fd610fa30334da6729b709104b22
Michael Lambert <mlambert125@live.com>
2024-01-17 13:58:16 -0500

    Used local labels, per Evie advice.  Also added done variable

--- src/hello-world.asm
+++ src/hello-world.asm
@@ -35,146 +35,164 @@ EntryPoint:
 	ld a, 0         ; 0 = off, 80 = on
 	ld [rNR52], a   ; rNR52 is the audio control register 
 
-WaitVBlank:
+.WaitVBlank:
 	ld a, [rLY]      ; rLY is the current scanline
 	cp SCRN_Y        ; compare current to the last scanline 
-	jp c, WaitVBlank ; if the comparison set the carry flag, then the comparison failed, so we're not in VBlank yet
+	jp c, .WaitVBlank ; if the comparison set the carry flag, then the comparison failed, so we're not in VBlank yet
 
 	; Turn the LCD off
 	ld a, LCDCF_OFF 
 	ld [rLCDC], a   ; rLCDC is the LCD control register
     
-CopyTiles:
+.CopyTiles:
 	; Copy the tile data to VRAM
 	ld de, Tiles
 	ld hl, _VRAM9000
 	ld bc, TilesEnd - Tiles
-CopyTilesLoop:
+.CopyTilesLoop:
 	ld a, [de]
 	ld [hli], a
 	inc de
 	dec bc
 	ld a, b
 	or a, c
-	jp nz, CopyTilesLoop
+	jp nz, .CopyTilesLoop
 
-ClearTilemap:
+.ClearTilemap:
     ld hl, _SCRN0  ; Start of tilemap
     ld bc, 1024 ; Countdown from 1024 to 0
-ClearTilemapLoop:
+.ClearTilemapLoop:
     ld a, 0     ; Load 0 into A (have to do this because ldi doesn't accept immediate values)
     ld [hli], a ; Write 0 to the tilemap VRAM and increment hl to point to the next tile VRAM location
     dec bc      ; decrement the countdown
     ld a, b     ; load the high byte of bc into A
     or a, c     ; logical OR the high byte with the low byte. If the result is 0, then bc is 0
-    jp nz, ClearTilemapLoop ; Loop if we aren't at zero yet on the count down
+    jp nz, .ClearTilemapLoop ; Loop if we aren't at zero yet on the count down
 
+.TurnOnLCD:
 	; Turn the LCD on
 	ld a, LCDCF_ON | LCDCF_BGON
 	ld [rLCDC], a
 
+.InitPallette:
 	; During the first (blank) frame, initialize pallette 
     ; Display intensity for each pallette color
 	ld a, PAL_DEFAULT 
 	ld [rBGP], a
 
+.InitFrameCount:
     ; Set wFrameCount variable to 0
     ld a, 0
     ld [wFrameCount], a
 
 
-    
+
 ; *********************************************************************************************************************
 ;   Main Loop
 ; *********************************************************************************************************************
 Main:
-WaitForNonVBlankLoop:
+.WaitForNonVBlankLoop:
     ; Wait until it's *not* VBlank to get on a new cycle
     ld a, [rLY] ; rLY is the current scanline
     cp SCRN_Y   ; compare current to the last non-vblank scanline
-    jp nc, WaitForNonVBlankLoop
-WaitVBlankMainLoop:
+    jp nc, .WaitForNonVBlankLoop
+.WaitVBlankMainLoop:
     ; Wait until it *is* VBlank
 	ld a, [rLY]
 	cp SCRN_Y  
-	jp c, WaitVBlankMainLoop
+	jp c, .WaitVBlankMainLoop
 
-UpdateState:
+; if title done, jump back to main (nothing really to do here)
+    ld a, [wTitleDone]
+    cp 1
+    jp z, Main   
+
+.UpdateState:
     ld a, [wFrameCount]
 
-ShowA1:
+.ShowA1:
     ; Jump to next if not on frame 0 (if we're here, register A holds the frame count)
     cp 0
-    jp nz, ShowB1
+    jp nz, .ShowB1
 
     ; Put TILE_A at position row 8, column 7 
     ld a, TILE_A 
     ld [_SCRN0 + (ROW_OFFSET * 8) + (COL_OFFSET * 7)], a
-    jp IncrementFrameCount
+    jp .IncrementFrameCount
 
-ShowB1:
+.ShowB1:
     ; Jump to next if not on frame 16 (if we're here, register A holds the frame count)
     cp 16  
-    jp nz, ShowA2
+    jp nz, .ShowA2
 
     ; Put TILE_B at position row 8, column 8
     ld a, TILE_B 
     ld [_SCRN0 + (ROW_OFFSET * 8) + (COL_OFFSET * 8)], a
-    jp IncrementFrameCount
+    jp .IncrementFrameCount
 
-ShowA2:
+.ShowA2:
     ; Jump to next if not on frame 32 (if we're here, register A holds the frame count)
     cp 32 
-    jp nz, ShowC
+    jp nz, .ShowC
 
     ; Put TILE_A at position row 8, column 9
     ld a, TILE_A 
     ld [_SCRN0 + (ROW_OFFSET * 8) + (COL_OFFSET * 9)], a
-    jp IncrementFrameCount
+    jp .IncrementFrameCount
 
-ShowC:
+.ShowC:
     ; Jump to next if not on frame 48 (if we're here, register A holds the frame count)
     cp 48 
-    jp nz, ShowA3
+    jp nz, .ShowA3
 
     ; Put TILE_C at position row 8, column 10
     ld a, TILE_C 
     ld [_SCRN0 + (ROW_OFFSET * 8) + (COL_OFFSET * 10)], a
-    jp IncrementFrameCount
+    jp .IncrementFrameCount
 
-ShowA3:
+.ShowA3:
     ; Jump to next if not on frame 64 (if we're here, register A holds the frame count)
     cp 64
-    jp nz, ShowB2
+    jp nz, .ShowB2
 
     ; Put TILE_A at position row 8, column 11
     ld a, TILE_A 
     ld [_SCRN0 + (ROW_OFFSET * 8) + (COL_OFFSET * 11)], a
-    jp IncrementFrameCount
+    jp .IncrementFrameCount
 
-ShowB2:
+.ShowB2:
     ; Jump to next if not on frame 80 (if we're here, register A holds the frame count)
     cp 80
-    jp nz, IncrementFrameCount 
+    jp nz, .IncrementFrameCount 
 
-    ; Put TILE_B at position row 8, column 11
+    ; Put TILE_B at position row 8, column 12
     ld a, TILE_B 
     ld [_SCRN0 + (ROW_OFFSET * 8) + (COL_OFFSET * 12)], a
-    jp IncrementFrameCount
 
-IncrementFrameCount:
+    ; Set title done
+    ld a, 1
+    ld [wTitleDone], a
+
+    jp .IncrementFrameCount
+
+.IncrementFrameCount:
     ld a, [wFrameCount]
     inc a
     ld [wFrameCount], a
 
-EndOfMain:
+.EndOfMain:
 	jp Main 
+
+
 
 ; *********************************************************************************************************************
 ;  Variables 
 ; *********************************************************************************************************************
 Section "Variables", WRAM0[$C000]
     wFrameCount: db     ; Rolling frame count (0-255)
+    wTitleDone: db      ; Flag to indicate if the title screen is done
+
+
 
 ; *********************************************************************************************************************
 ;   Static Data
